ed0bb57f0956252b9e030f2b225bb29f9b5c059a
Khalil <khalil@pop-os.localdomain>
2021-07-19 16:58:23 +0300

    inital main

--- main.c
+++ main.c
@@ -1,33 +1,18 @@
-/*
- * =====================================================================================
- *
- *       Filename:  main.c
- *
- *    Description:  my_readline for file reading 
- *
- *        Version:  1.0
- *        Created:  07/14/2021 09:37:26 AM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  Khalil 
- *   Organization:  
- *
- * =====================================================================================
- */
-
 #include "include/my_readline.h"
 
 int main(){
-    
-    char *ret_str;
+
+    char *ret_str ;
     int file = open("./text.txt", O_RDONLY);
 
-    while((ret_str = my_readline(file)) != NULL){
+    if(file < 0)
+        return 1;
+
+    while((ret_str = my_readline(file))!= NULL){
         printf("%s\n", ret_str);
-        free(ret_str);
     }
-    
+
+    free(ret_str);
     close(file);
     return 0;
 }
