f9dca6b97c240993dc09ea11d5d0bac043be6b30
Khalil <khalil@pop-os.localdomain>
2021-07-19 17:01:05 +0300

    Readline functions

--- my_readline.c
+++ my_readline.c
@@ -1,8 +1,31 @@
 #include "include/my_readline.h"
 
-char *my_readline(int fd){
-    char *buffer;
-    
-    if(!(buffer = malloc))
 
+
+char *my_readline(int fd){
+
+    static char buffer[READLINE_READ_SIZE];
+    
+    char *temp = malloc(1 * sizeof(char));
+
+    int index = 0;
+    buffer[index] = '\0';
+
+    while(index < READLINE_READ_SIZE){
+        
+        if(read(fd, temp,1) <= 0){
+            free(temp);
+            return NULL;
+        }
+       
+        strcat(buffer, &temp[0]);
+
+        if(temp[0] == '\n')
+            break;
+        
+        index++;
+    }
+
+    free(temp);
+    return buffer;
 }
