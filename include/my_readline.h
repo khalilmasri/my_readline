d2bc2c56257421610e9b230e6393e0d3791223de
Khalil <khalil@pop-os.localdomain>
2021-07-20 16:30:48 +0300

    header_file

--- include/my_readline.h
+++ include/my_readline.h
@@ -1,12 +1,27 @@
+#ifndef MY_READLINE_H
+#define MY_READLINE_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 
 #define READLINE_READ_SIZE 10
 
-char *my_readline(int fd);
+char *my_readline(int fd, char *buffer);
+void create_stdin(int fd, char **argv, int argc);
 
+#endif
+
+/* ------------------------------------------------------------- */
+
+#ifndef MY_STRING_H
+#define MY_STRING_H
+
+char *my_strcat(char *dest, char *src);
+char *my_strcpy(char *dest, char *src);
+size_t my_strlen(char* str);
+
+#endif
