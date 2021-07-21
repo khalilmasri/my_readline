a4acd3469ba756fc7f9d8edd94c89ed49022d879
Khalil <khalil@pop-os.localdomain>
2021-07-21 11:22:36 +0300

    readline_functions

--- src/my_readline.c
+++ src/my_readline.c
@@ -1,45 +1,32 @@
 #include "../include/my_readline.h"
 
-int get_size(char **argv){
+option_t check_option(int argc, char **argv){
+    
+    if(argc > 3)
+        return ERROROPT;
 
-    int index = 0,
-        size = 0;
-
-    while(argv[index]){
-        size += my_strlen(argv[index]);
-        index++;
+    if(argv[1][0] == '-'){
+        if(my_strlen(argv[1]) == 2){
+            switch (argv[1][1]){
+                case 'f':
+                    return f;
+                    break;
+                case 's':
+                    return s;
+                    break;
+                default:
+                    return ERROROPT;
+                    break;
+            }
+        }else{
+            return ERROROPT;
+        }
     }
-
-    return size+1;
+    return ERROROPT;
 }
-
-void create_stdin(int fd, char **argv, int argc){
-
-    int index = 1;
-    int size = get_size(argv)+argc;
-
-    char *ret_str = malloc((sizeof(char)*size));
-
-    while(argv[index]){
-
-        if(index > 1)
-            my_strcat(ret_str ," ");
-
-        my_strcat(ret_str, argv[index]);
-
-        index++;
-    }
-
-    my_strcat(ret_str, "\0");
-
-    write(fd, ret_str, size);
-    free(ret_str);
-}
-
 
 char *my_readline(int fd, char *buffer){
 
-    
     char *temp = malloc(1 * sizeof(char));
 
     int index = 0;
